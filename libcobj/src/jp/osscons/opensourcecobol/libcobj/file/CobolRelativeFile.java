992c825b8ab8f6fce396a6ba6050263224bcd886
kio-watatanabe <ki-watanabe@tsh-world.co.jp>
2023-01-26 15:53:49 +0900

    Create start method for relative file

--- libcobj/src/jp/osscons/opensourcecobol/libcobj/file/CobolRelativeFile.java
+++ libcobj/src/jp/osscons/opensourcecobol/libcobj/file/CobolRelativeFile.java
@@ -18,9 +18,17 @@
  */
 package jp.osscons.opensourcecobol.libcobj.file;
 
+import java.io.*;
+import java.nio.channels.*;
 import jp.osscons.opensourcecobol.libcobj.data.AbstractCobolField;
 
 public class CobolRelativeFile extends CobolFile {
+
+  protected static final int COB_EQ = 1;
+  protected static final int COB_LT = 2;
+  protected static final int COB_LE = 3;
+  protected static final int COB_GT = 4;
+  protected static final int COB_GE = 5;
 
   public CobolRelativeFile(
       String select_name,
@@ -77,20 +85,59 @@ public class CobolRelativeFile extends CobolFile {
   }
 
   @Override
-  public int open_(String filename, int mode, int sharing) {
-    System.out.println("Relative.open");
-    return 0;
-  }
-
-  @Override
-  public int close_(int opt) {
-    System.out.println("Relative.close");
-    return 0;
-  }
-
-  @Override
   public int start_(int cond, AbstractCobolField key) {
-    System.out.println("Relative.start");
+    int kindex;
+    int relsize;
+    int off;
+
+    /* get the index */
+    kindex = key.getInt() - 1;
+    relsize = this.record_max + this.record_size.getInt();
+    if (cond == COB_LT) {
+      kindex--;
+    } else if (cond == COB_GT) {
+      kindex++;
+    }
+
+    /* seek the index */
+    int offset = 0;
+    RandomAccessFile file;
+    try {
+      file = new RandomAccessFile(this.assign.fieldToString(), "r");
+      for (; ; ) {
+        off = kindex * relsize;
+        try {
+          file.seek((long) off);
+
+          if (file.getFilePointer() - file.length() != 0
+              || file.read(this.record_size.getBytes(), offset, this.record_size.getInt()) != 1) {
+            return COB_STATUS_23_KEY_NOT_EXISTS;
+          }
+
+          /* check if a valid record */
+          if (this.record_size.getInt() > 0) {
+            key.setInt(kindex + 1);
+            file.seek(file.getFilePointer() - this.record_size.getLong());
+            return COB_STATUS_00_SUCCESS;
+          }
+        } catch (IOException e) {
+        }
+        switch (cond) {
+          case COB_EQ:
+            return COB_STATUS_23_KEY_NOT_EXISTS;
+          case COB_LT:
+          case COB_LE:
+            kindex--;
+            break;
+          case COB_GT:
+          case COB_GE:
+            kindex++;
+            break;
+        }
+      }
+
+    } catch (FileNotFoundException e) {
+    }
     return 0;
   }
 
