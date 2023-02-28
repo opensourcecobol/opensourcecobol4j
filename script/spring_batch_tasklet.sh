943a72e19eda1dfb10f42c51c128a3e21b4ee46e
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2023-02-28 13:16:14 +0900

    add: add help messages to cobj and add comments to spring_batch_tasklet.sh

--- script/spring_batch_tasklet.sh
+++ script/spring_batch_tasklet.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This is an example of -edit-code-command.
+# If you run the following command, cobj generates java source code compatible for Spring Batch.
+# cobj -C -edit-code-command="bash spring_batch_tasklet.sh" <<<your COBOL source file>>>
+
+# parse command line options with getopts
 while getopts c:t-: opt; do
     optarg="$OPTARG"
     if [[ "$opt" = - ]]; then
@@ -27,11 +32,14 @@ while getopts c:t-: opt; do
 done
 shift $((OPTIND - 1))
 
+
 case "$target" in
+    # when `--target=main-class` is specified
     main-class-annotation)
         echo "@Component"
 	    exit 0
 	    ;;
+    # when `--target=file-header` is specified
     file-header)
         echo 'import org.slf4j.Logger;'
         echo 'import org.slf4j.LoggerFactory;'
@@ -42,10 +50,12 @@ case "$target" in
         echo 'import org.springframework.stereotype.Component;'
         exit 0
         ;;
+    # when `--target=main-class` is specified
     main-class-implements)
         echo -n 'Tasklet'
         exit 0
         ;;
+    # when `--target=main-class` is specified
     main-class-contents)
         echo '  @Override'
         echo '  public RepeatStatus execute(StepContribution conribution, ChunkContext chunkContext) throws Exception {'
