4e4d4037f4ebf8f8182811354a6ad0c7d410f16b
root <root@MSPC203.tsh.local>
2023-09-27 12:47:12 +0900

    formatted

--- libcobj/src/jp/osscons/opensourcecobol/libcobj/common/CobolIntrinsic.java
+++ libcobj/src/jp/osscons/opensourcecobol/libcobj/common/CobolIntrinsic.java
@@ -2031,12 +2031,18 @@ public class CobolIntrinsic {
 
   /**
    * cob_intr_fraction_partの実装
+   *
    * @param srcfield
    * @return
    */
-  public static AbstractCobolField funcFractionPart(AbstractCobolField srcfield){
+  public static AbstractCobolField funcFractionPart(AbstractCobolField srcfield) {
     CobolFieldAttribute attr =
-        new CobolFieldAttribute(CobolFieldAttribute.COB_TYPE_NUMERIC_BINARY, 18, 18, CobolFieldAttribute.COB_FLAG_HAVE_SIGN, null);
+        new CobolFieldAttribute(
+            CobolFieldAttribute.COB_TYPE_NUMERIC_BINARY,
+            18,
+            18,
+            CobolFieldAttribute.COB_FLAG_HAVE_SIGN,
+            null);
     AbstractCobolField field = CobolFieldFactory.makeCobolField(8, (CobolDataStorage) null, attr);
     makeFieldEntry(field);
 
