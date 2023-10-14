d42c3b543c227cb245dba283799980ad59300ec6
yutaro-sakamoto <80912876+yutaro-sakamoto@users.noreply.github.com>
2023-10-14 21:24:20 +0900

    Use Gradle (#235)

---
+++ libcobj/app/build.gradle.kts
@@ -0,0 +1,36 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:31.1-jre")
+
+    implementation("org.xerial:sqlite-jdbc:3.30.1")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+
+application {
+    mainClass.set("")
+}
+
+tasks.withType<Jar>().configureEach {
+    archiveBaseName.set("libcobj")
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+}
---
+++ libcobj/settings.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+rootProject.name = "opensource COBOL 4J libcobj"
+include("app")
