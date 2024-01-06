6f492876160e943ad69b78ee3a18367479298c0d
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-01-07 02:31:50 +0900

    [Refactor]: format source file

--- libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/indexed_file/IndexedFileUtilMain.java
+++ libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/indexed_file/IndexedFileUtilMain.java
@@ -1,7 +1,7 @@
 package jp.osscons.opensourcecobol.libcobj.user_util.indexed_file;
 
 public class IndexedFileUtilMain {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
