52ec610e3dbecc9b578023ae174c041d100df032
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2024-05-07 11:40:16 +0900

    Add files for windows

--- win/config.h
+++ win/config.h
@@ -299,22 +299,22 @@
 #define ICONV_CONST 
 
 /* Name of package */
-#define PACKAGE "opensource-cobol-1.5.2J"
+#define PACKAGE "opensource-cobol4j-1.0.22"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "ws-opensource-cobol-contact@osscons.jp"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "opensource COBOL"
+#define PACKAGE_NAME "opensource COBOL 4J"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "opensource COBOL 1.5.2J"
+#define PACKAGE_STRING "opensource COBOL 4J 1.0.22"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "opensource-cobol-1.5.2J"
+#define PACKAGE_TARNAME "opensource-cobol4j-1.0.22"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.5.2"
+#define PACKAGE_VERSION "1.0.22"
 
 /* Define a patch level */
 #define PATCH_LEVEL 0
