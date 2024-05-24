98b9c4e601e0e3df2916782d4627a9fd82262647
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2024-05-24 12:05:36 +0900

    Fix a ps1 file

--- win/make-install.ps1
+++ win/make-install.ps1
@@ -1,7 +1,7 @@
 mkdir C:\opensourcecobol4j\bin\
 mkdir C:\opensourcecobol4j\config\
 
-set PATH=%PATH%;C:\opensourcecobol4j\bin\;C:\opensourcecobol4j\config\
+$env:PATH+=";C:\opensourcecobol4j\bin\;C:\opensourcecobol4j\config\"
 
 copy .\x64\Debug\cobj.exe C:\opensourcecobol4j\bin\
 copy ..\config\*.conf C:\opensourcecobol4j\config\
