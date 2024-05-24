8d499e17b7dbb402bd22bdd3f872c7eccfff2bab
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2024-05-24 11:03:40 +0900

    Fix a ps1 file

--- win/make-install.ps1
+++ win/make-install.ps1
@@ -1,5 +1,5 @@
 mkdir C:\opensourcecobol4j\bin\
 mkdir C:\opensourcecobol4j\config\
 
-copy .\win\x64\Debug\cobj.exe C:\opensourcecobol4j\bin\
+copy .\x64\Debug\cobj.exe C:\opensourcecobol4j\bin\
 copy ..\config\*.conf C:\opensourcecobol4j\config\
