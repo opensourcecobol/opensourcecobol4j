b7d0ed34b7457e72d8b1d26a9f87f45f1a242091
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2024-06-03 11:26:00 +0900

    Added Windows environment variable settings

--- win/make-install.ps1
+++ win/make-install.ps1
@@ -1,7 +1,5 @@
 mkdir C:\opensourcecobol4j\bin\
 mkdir C:\opensourcecobol4j\config\
 
-$env:PATH+=";C:\opensourcecobol4j\bin\;C:\opensourcecobol4j\config\"
-
 copy .\x64\Release\cobj.exe C:\opensourcecobol4j\bin\
 copy ..\config\*.conf C:\opensourcecobol4j\config\
