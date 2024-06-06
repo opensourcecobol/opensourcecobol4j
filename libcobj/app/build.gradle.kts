87ee36c647e91a4d03bdeb72408544c06bb07b9a
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2024-06-06 13:47:16 +0900

    [Upgrade]: upgrade org.junit.jupiter:junit-jupiter

--- libcobj/app/build.gradle.kts
+++ libcobj/app/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("com.google.guava:guava:31.1-jre")
     implementation("org.xerial:sqlite-jdbc:3.30.1")
     implementation("commons-cli:commons-cli:1.8.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.json:json:20231013")
     spotbugs("com.github.spotbugs:spotbugs:4.8.5")
