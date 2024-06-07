45d25f3f745b541087613317623d4ef75f92d1ec
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2024-06-07 15:51:11 +0900

    [Upgarade]: upgrade to 33.2.1

--- libcobj/app/build.gradle.kts
+++ libcobj/app/build.gradle.kts
@@ -27,7 +27,7 @@ tasks {
 }
 
 dependencies {
-    implementation("com.google.guava:guava:33.2-jre")
+    implementation("com.google.guava:guava:33.2.1-jre")
     implementation("org.xerial:sqlite-jdbc:3.30.1")
     implementation("commons-cli:commons-cli:1.8.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
