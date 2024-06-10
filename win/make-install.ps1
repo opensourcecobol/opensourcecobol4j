3ec3857bdd8b582d9e8341a303e206daec97b767
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2024-06-10 17:51:55 +0900

    Fix powershell script

--- win/make-install.ps1
+++ win/make-install.ps1
@@ -3,5 +3,5 @@ mkdir C:\opensourcecobol4j\lib\
 mkdir C:\opensourcecobol4j\config\
 
 copy .\x64\Release\cobj.exe C:\opensourcecobol4j\bin\
-copy .\libcobj.jar C:\opensourcecobol4j\lib\
+copy ..\libcobj\app\build\libs\libcobj.jar C:\opensourcecobol4j\lib\
 copy ..\config\*.conf C:\opensourcecobol4j\config\
