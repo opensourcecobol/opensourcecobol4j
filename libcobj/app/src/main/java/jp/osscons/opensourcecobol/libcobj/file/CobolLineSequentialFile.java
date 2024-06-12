e77fc65398f852a7c011dc4cefa630fb4f2f0c67
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-06-12 22:02:12 +0900

    [WIP]: wip

--- libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/file/CobolLineSequentialFile.java
+++ libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/file/CobolLineSequentialFile.java
@@ -23,8 +23,38 @@ import jp.osscons.opensourcecobol.libcobj.data.CobolDataStorage;
 import jp.osscons.opensourcecobol.libcobj.exceptions.CobolRuntimeException;
 import jp.osscons.opensourcecobol.libcobj.exceptions.CobolStopRunException;
 
+/** TODO: 準備中 */
 public class CobolLineSequentialFile extends CobolFile {
 
+  /**
+   * TODO: 準備中
+   *
+   * @param selectName TODO: 準備中
+   * @param fileStatus TODO: 準備中
+   * @param assign TODO: 準備中
+   * @param record TODO: 準備中
+   * @param recordSize TODO: 準備中
+   * @param recordMin TODO: 準備中
+   * @param recordMax TODO: 準備中
+   * @param nkeys TODO: 準備中
+   * @param keys TODO: 準備中
+   * @param organization TODO: 準備中
+   * @param accessMode TODO: 準備中
+   * @param lockMode TODO: 準備中
+   * @param openMode TODO: 準備中
+   * @param flagOptional TODO: 準備中
+   * @param lastOpenMode TODO: 準備中
+   * @param special TODO: 準備中
+   * @param flagNonexistent TODO: 準備中
+   * @param flagEndOfFile TODO: 準備中
+   * @param flagBeginOfFile TODO: 準備中
+   * @param flagFirstRead TODO: 準備中
+   * @param flagReadDone TODO: 準備中
+   * @param flagSelectFeatures TODO: 準備中
+   * @param flagNeedsNl TODO: 準備中
+   * @param flagNeedsTop TODO: 準備中
+   * @param fileVersion TODO: 準備中
+   */
   public CobolLineSequentialFile(
       String selectName,
       byte[] fileStatus,
