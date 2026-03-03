b7731c3c512c3b6e07065c8c5b5ccacbc34011d1
tsh-hashimoto <ta-hashimoto@tsh-world.co.jp>
2026-03-03 07:23:41 +0000

    fix: apply Copilot review

--- cobj/codegen.c
+++ cobj/codegen.c
@@ -3033,6 +3033,7 @@ static void joutput_call(struct cb_call *p) {
   joutput(" (");
   for (l = p->args, n = 1; l; l = CB_CHAIN(l), n++) {
     x = CB_VALUE(l);
+    field_iteration = (int)n - 1;
     switch (CB_PURPOSE_INT(l)) {
     case CB_CALL_BY_REFERENCE:
       if (CB_NUMERIC_LITERAL_P(x) || CB_BINARY_OP_P(x)) {
